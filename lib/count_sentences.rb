f6a52acc221ff714eca787b403228a3242223a43
Tami <catquarks@gmail.com>
2016-07-28 13:30:36 -0400

    monkey patched string class  to recognize and count sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,43 @@ require 'pry'
 
 class String
 
-  def sentence?
+  def sentence? # returns true if string ends in period
+
+		self.end_with?(".") ? true : false
     
   end
 
   def question?
 
+  	self.end_with?("?") ? true : false
+
   end
 
   def exclamation?
 
+  	self.end_with?("!") ? true : false
+
   end
+
 
   def count_sentences
 
+  	# binding.pry
+
+  	num_of_sentences = self.split(/[.|!|?]+/).size
+
+  	num_of_sentences == 3 ? num_of_sentences : 0
+
+  	# binding.pry
+
+  	num_of_sentences
+
   end
 end
+
+# binding.pry
+
+# "Moose is a sentence. No it's not! Oops?".count_sentences
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+complex_string.count_sentences
